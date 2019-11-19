b17239d7bb5e35b373da463e5b155d200c2f17cb
Hongjie Zhu <hongjie.zhu@mediakind.com>
2019-11-19 19:39:10 +0800

    tm

--- lib/page-objects/GetInfoPage.ts
+++ lib/page-objects/GetInfoPage.ts
@@ -14,38 +14,75 @@ export class GetInfoPage extends Atom {
             selector: "#J_goodsList ul li",
             locateStrategy: "css selector"
         },
-        nextButton: {
+        tm_list: {
+            selector: "#J_ItemList .product",
+            locateStrategy: "css selector"
+        },
+        t_list: {
             selector: "//div[@id='AspNetPager1']/a[contains(text(), '下一页')]",
             locateStrategy: "xpath"
         }
     };
 
-    public getJD_DataList() {
+    public getJD_DataList(sortBy: string, callback: any) {
         this.api.useCss().waitForElementVisible(this.elements["jd_list"].selector);
-        this.api.execute(() => {
+        this.api.execute((sortBy: string) => {
             const items = [];
             const itemLenght = document.querySelectorAll("#J_goodsList ul li").length - 1;
 
             for (let index = 0; index < itemLenght; index++) {
                 const jd_price = document.querySelector("#J_goodsList ul li:nth-child(" + (index + 1) + ") .p-price").textContent.trim();
-                const jd_name = document.querySelector("#J_goodsList ul li:nth-child(" + (index + 1) + ") .p-name").textContent.trim().replace(/\r|\n|\t/g, "");
-                const jd_commit = document.querySelector("#J_goodsList ul li:nth-child(" + (index + 1) + ") .p-commit").textContent.trim();
+                const jd_name = document.querySelector("#J_goodsList ul li:nth-child(" + (index + 1) + ") .p-name").textContent.trim().replace(/\r|\n|\t/g, " ");
+                const jd_commit = document.querySelector("#J_goodsList ul li:nth-child(" + (index + 1) + ") .p-commit").textContent.trim().replace(/\r|\n|\t/g, " ");
                 const jd_shop = document.querySelector("#J_goodsList ul li:nth-child(" + (index + 1) + ") .p-shop").textContent.trim();
                 const jd_url = "https:" + document.querySelector("#J_goodsList ul li:nth-child(" + (index + 1) + ") .p-img a").getAttribute("href").trim();
 
                 const y = {
+                    website: "JD.com",
                     price: jd_price,
                     name: jd_name,
                     commit: jd_commit,
                     shop: jd_shop,
-                    url: jd_url
+                    url: jd_url,
+                    sortBy: sortBy
+                };
+                items.push(y);
+            }
+            return JSON.stringify(items);
+        }, [sortBy], (result: CallbackResult) => {
+            callback(result.value);
+        });
+    }
+
+    public getTM_DataList(sortBy: string, callback: any) {
+        this.api.useCss().waitForElementVisible(this.elements["tm_list"].selector);
+        this.api.execute(() => {
+            const items = [];
+            const itemLenght = document.querySelectorAll("#J_ItemList .product").length - 1;
+
+            for (let index = 0; index < itemLenght; index++) {
+                const tm_price = document.querySelector("#J_ItemList div:nth-child(" + (index + 1) + ") .productPrice").textContent.trim();
+                const tm_name = document.querySelector("#J_ItemList div:nth-child(" + (index + 1) + ") .productTitle ").textContent.trim().replace(/\r|\n|\t/g, " ");
+                const tm_deal = document.querySelector("#J_ItemList div:nth-child(" + (index + 1) + ") .productStatus span:nth-child(1)").textContent.trim().replace(/\r|\n|\t/g, " ");
+                const tm_commit = document.querySelector("#J_ItemList div:nth-child(" + (index + 1) + ") .productStatus span:nth-child(2)").textContent.trim().replace(/\r|\n|\t/g, " ");
+                const tm_shop = document.querySelector("#J_ItemList div:nth-child(" + (index + 1) + ") .productShop").textContent.trim();
+                const tm_url = "https:" + document.querySelector("#J_ItemList div:nth-child(" + (index + 1) + ") .productImg-wrap a").getAttribute("href").trim();
+
+                const y = {
+                    website: "TM.com",
+                    price: tm_price,
+                    name: tm_name,
+                    commit: tm_commit,
+                    deal: tm_deal,
+                    shop: tm_shop,
+                    url: tm_url,
+                    sortBy: sortBy
                 };
                 items.push(y);
             }
             return JSON.stringify(items);
         }, [], (result: CallbackResult) => {
-            console.log(result);
-            return result.value;
+            callback(result.value);
         });
     }
 
--- tests/Crowler/JingDong_TianMao.ts
+++ tests/Crowler/JingDong_TianMao.ts
@@ -7,6 +7,10 @@ import { NightWatchBrowser } from "../../typings/nightwatch/nightwatch";
 const setup = require("../../lib/common/setup");
 
 const searchKey = "台式机";
+let jd_list: any[] = [];
+let tm_list: any[] = [];
+const userName = "15779681353";
+const password = "Zhuhong969632907";
 
 module.exports = {
     tags: ["TJ"],
@@ -32,15 +36,29 @@ module.exports = {
 
     "Search the results and get the data for JD."(browser: NightWatchBrowser) {
         const getDataPage = browser.page.GetInfoPage();
-
         browser.waitForElementVisible("#key");
         browser.setValue("#key", searchKey);
         browser.click(".search-m .form button:nth-child(5)");
         browser.waitForElementVisible("#J_goodsList");
-        const lists = getDataPage.getJD_DataList();
-        // 销量
-        browser.click("#J_filter .f-line .f-sort  a:nth-child(2)");
-        browser.waitForElementNotPresent("#J_loading");
+        getDataPage.getJD_DataList("zonghe", (lists: string) => {
+            // sortBy 综合
+            const zonghe_list = JSON.parse(lists);
+            jd_list = jd_list.concat(zonghe_list);
+            // sortBy 销量
+            browser.click("#J_filter .f-line .f-sort  a:nth-child(2)");
+            browser.waitForElementNotPresent("#J_loading");
+            getDataPage.getJD_DataList("xiaoliang", (lists: string) => {
+                const xiaoliang_list = JSON.parse(lists);
+                jd_list = jd_list.concat(xiaoliang_list);
+                // sortBy 评论
+                browser.click("#J_filter .f-line .f-sort  a:nth-child(3)");
+                browser.waitForElementNotPresent("#J_loading");
+                getDataPage.getJD_DataList("pinglun", (lists: string) => {
+                    const xinpin_list = JSON.parse(lists);
+                    jd_list = jd_list.concat(xinpin_list);
+                });
+            });
+        });
     },
 
     "Go to TM for search"(browser: NightWatchBrowser) {
@@ -57,6 +75,37 @@ module.exports = {
     },
 
     "Search the results and get the data for TM."(browser: NightWatchBrowser) {
+        const getDataPage = browser.page.GetInfoPage();
+        browser.waitForElementVisible("#mq");
+        browser.click("#login-info .sn-login");
+        browser.waitForElementVisible("#J_LoginBox");
+        browser.setValue("#TPL_username_1", userName);
+        browser.setValue("#TPL_password_1", password);
+        browser.click("#J_SubmitStatic");
 
+        browser.waitForElementVisible("#mq");
+        browser.setValue("#mq", searchKey);
+        browser.click("#mallSearch .mallSearch-input button:nth-child(3)");
+        browser.waitForElementVisible("#J_ItemList");
+        getDataPage.getTM_DataList("zonghe", (lists: string) => {
+            // sortBy 综合
+            const zonghe_list = JSON.parse(lists);
+            tm_list = tm_list.concat(zonghe_list);
+            console.log(tm_list);
+            // sortBy 销量
+            // browser.click("#J_filter a:nth-child(4)");
+            // browser.waitForElementNotPresent("#J_loading");
+            // getDataPage.getTM_DataList("xiaoliang", (lists: string) => {
+            //     const xiaoliang_list = JSON.parse(lists);
+            //     tm_list = tm_list.concat(xiaoliang_list);
+            //     // sortBy 评论
+            //     browser.click("#J_filter a:nth-child(2)");
+            //     browser.waitForElementNotPresent("#J_loading");
+            //     getDataPage.getTM_DataList("pinglun", (lists: string) => {
+            //         const xinpin_list = JSON.parse(lists);
+            //         tm_list = tm_list.concat(xinpin_list);
+            //     });
+            // });
+        });
     }
 };
